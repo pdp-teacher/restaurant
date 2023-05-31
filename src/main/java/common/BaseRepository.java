ca5063c46d9fb5593036c56d46229d9396c6958e
Abdurazzoq✔ <abdurazzoqrazzoqov007@gmail.com>
2023-05-31 09:54:49 +0500

    Abdurazzoq

---
+++ src/main/java/common/BaseRepository.java
@@ -0,0 +1,58 @@
+package common;
+
+import java.io.*;
+import java.util.List;
+
+public abstract class BaseRepository <ENTITY extends  BaseEntity<ID>,ID> implements Repository<ENTITY,ID>{
+    protected abstract String getFilePath();
+    @Override
+    public ENTITY save(ENTITY entity) {
+        return null;
+    }
+
+    @Override
+    public ENTITY findById(ID id) {
+        return null;
+    }
+
+    @Override
+    public List<ENTITY> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(ID id) {
+
+    }
+   /* protected List<ENTITY>readFromFile(){
+        try(ObjectInputStream stream = new ObjectInputStream(new FileInputStream(getFilePath())))
+    }*/
+
+    /*protected List<ENTITY> readFromFile()
+    {
+        try( ObjectInputStream stream = new ObjectInputStream( new FileInputStream( getFilePath() ) ) )
+        {
+            return (List<ENTITY>) stream.readObject();
+        }
+        catch( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+        catch( ClassNotFoundException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }*/
+
+    protected void writeToFile( List<ENTITY> entities )
+    {
+        try( ObjectOutputStream stream = new ObjectOutputStream( new FileOutputStream( getFilePath() ) ) )
+        {
+            stream.writeObject( entities );
+        }
+        catch( IOException e )
+        {
+            e.printStackTrace();
+        }
+    }
+}
